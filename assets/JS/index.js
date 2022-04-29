6c913cbb6598147ff8cd990fb3722741ebf978e8
Alexandre Bastos <xande.bastos321@gmail.com>
2022-04-29 20:44:08 -0300

    Iniciando as funções JS

---
+++ assets/JS/index.js
@@ -0,0 +1,11 @@
+function EscolherPersonagem() {
+    let escolha;
+    while (escolha != 1 && escolha != 2) {
+        escolha = prompt(`Deseja parar a música? 
+    SIM: 1
+    NÂO: 2`)
+    } 
+    if(escolha == 1) {
+        window.location.href = ("")
+    }
+}
