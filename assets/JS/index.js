ee770ef10fe843b0a7daf5c10b56af1a4c01579a
Alexandre Bastos <xande.bastos321@gmail.com>
2022-04-29 22:17:51 -0300

    Começando a declarar as funções JS

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,11 +1,4 @@
-function EscolherPersonagem() {
-    let escolha;
-    while (escolha != 1 && escolha != 2) {
-        escolha = prompt(`Deseja parar a música? 
-    SIM: 1
-    NÂO: 2`)
-    } 
-    if(escolha == 1) {
-        window.location.href = ("")
-    }
+function comecarJogo() {
+    
 }
+
